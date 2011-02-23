7c01b08286e86cc1d97d54d0bc682fdf6b050d84
man.stick <man.stick@c8a842cf-b2b2-00e4-abb7-77f575d9ef2f>
2011-02-23 21:06:22 +0000

    Added install SQL

---
+++ install/install.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `ssearch_content` (
+  `uid` varchar(32) NOT NULL,
+  `mime_type` varchar(64) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `protocol` varchar(10) NOT NULL,
+  `domain` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`uid`),
+  KEY `date` (`date`),
+  KEY `domain` (`domain`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
